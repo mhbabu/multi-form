d7367833bc1c8e3b96104ba3375a125e9f22753b
Mahadi Hassan Babu <write2babu.cse@gmail.com>
2023-09-29 03:37:31 +0600

    PDF - file modification

--- app/Http/Controllers/DocumentSignController.php
+++ app/Http/Controllers/DocumentSignController.php
@@ -86,7 +86,7 @@ class DocumentSignController extends Controller
 
         $pdf = new \Mpdf\Mpdf();
         $pdf->AddPage();
-        $pdf->SetHTMLFooter('<div style="position: absolute; bottom: 0; right: 1%; width: 100px; text-align: right;"><img src="' . $signatureImage . '" style="width: 100%;" />[NOTARY JURAT]</div>');
+        $pdf->SetHTMLFooter('<div style="position: absolute; bottom: 0; right: 1%; width: 80px; text-align: right;"><img src="' . $signatureImage . '" style="width: 100%;" />[NOTARY JURAT]</div>');
         $html = view('pdf_with_signature', ['data' => $request->all(), 'signatureImage' => $signatureImage])->render();
         $pdf->WriteHTML($html);
         $filename = 'generated_pdf_' . time() . '.pdf';
